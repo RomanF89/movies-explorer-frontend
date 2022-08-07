8ff66cd4ac85cbaafe5b79ff3a54bbcc8ca2fd91
Roman Faritov <romko_1992@mail.ru>
2022-08-07 18:28:32 +0500

    feat: Добавить функционал JSX

--- src/utils/MainApi.js
+++ src/utils/MainApi.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'http://localhost:3000';
+export const BASE_URL = 'https://movies89api.nomorepartiesxyz.ru';
 
 const checkResponse = (response) => {
   console.log('response ok: ', response);
